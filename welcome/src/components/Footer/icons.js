673924d7a016a8ac1873797218d1e888281cffe1
ruslan tasmukhanov <ruslahn.q@gmail.com>
2023-07-28 17:09:34 +0600

    fix: sever deploy

--- welcome/src/components/Footer/icons.js
+++
@@ -1,7 +0,0 @@
-import { ReactComponent as INSTAGRAM } from "../../assets/Icons/instagram.svg";
-import { ReactComponent as GITHUB } from "../../assets/Icons/github.svg";
-import { ReactComponent as LINKEDIN } from "../../assets/Icons/linkedin.svg";
-import { ReactComponent as TWITTER } from "../../assets/Icons/twitter.svg";
-import { ReactComponent as LOGO } from "../../assets/Icons/3logo.svg";
-
-export { INSTAGRAM, GITHUB, LINKEDIN, TWITTER, LOGO };
